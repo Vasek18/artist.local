b8ec4462febabdc63fe9fb3a53acdabe6ec4b7d5
Vasek18 <aristov-92@mail.ru>
2016-08-04 21:18:52 +0300

    Индексная страница

--- resources/views/pictures/index.blade.php
+++ resources/views/pictures/index.blade.php
@@ -3,28 +3,28 @@
 @section("content")
     <div class="container">
         <div class="row">
-            <div class="col-sm-6 col-md-4">
-                @foreach($pictures as $picture)
+            @foreach($pictures as $picture)
+                <div class="col-sm-6 col-md-4">
                     <div class="thumbnail">
-                        {{--<img src="..."--}}
-                        {{--alt="{{ $picture->name }}"--}}
-                        {{--title="{{ $picture->name }}">--}}
+                        <img src="{{ $picture->path_to_svg }}"
+                             alt="{{ $picture->name }}"
+                             title="{{ $picture->name }}">
                         <div class="caption">
                             <h3>{{ $picture->name }}</h3>
                             <p>
-                                <a href="{{ action('PictureController@show', [$picture->id]) }}"
+                                <a href="{{ action('PictureController@edit', [$picture->id]) }}"
                                    class="btn btn-primary"
                                    role="button">Редактировать
                                 </a>
                             </p>
                         </div>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
             <div class="col-sm-6 col-md-4">
                 <div class="thumbnail">
                     <a href="{{ action('PictureController@create') }}"
-                       class="btn btn-lg">
+                       class="btn btn-lg add-pic-btn">
                         <span class="glyphicon glyphicon-plus"
                               aria-hidden="true"></span>
                     </a>
